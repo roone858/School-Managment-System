1f51033c741c08c13acccfa96720cfe55800ecfb
roone858 <MahmoudG.dev@gmail.com>
2023-05-10 09:04:40 +0300

    remove auth

--- api/src/server.js
+++ api/src/server.js
@@ -15,9 +15,9 @@ app.use(bodyParser.json());
 // Use middleware to enable CORS
 app.use(cors());
 
-app.post("/auth/login", login);
+// app.post("/auth/login", login);
 
-app.use("/api", checkToken, apiRoute);
+app.use("/api", apiRoute);
 
 app.listen(4000, () => {
   console.log("server listening on port : 4000");
