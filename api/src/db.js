b987fb3f96dbfed9fbf994b7b4a8ee213d5b306d
roone858 <MahmoudG.dev@gmail.com>
2023-05-10 17:52:06 +0300

    ssl : true to solve tcp bug

--- api/src/db.js
+++ api/src/db.js
@@ -1,13 +1,14 @@
 const { Pool } = require("pg");
-require('dotenv').config()
+require("dotenv").config();
 
 // Create a pool of PostgreSQL connections
 const client = new Pool({
   user: process.env.DATABASE_USERNAME,
   host: process.env.HOST,
-  database:  process.env.DATABASE,
-  password:  process.env.PASSWORD,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
   port: process.env.PORT,
+  ssl: true,
 });
 
 module.exports = client;
