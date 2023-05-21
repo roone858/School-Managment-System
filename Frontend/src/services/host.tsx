90721df8f9b45f391912f2e12383e2e374bed739
roone858 <MahmoudG.dev@gmail.com>
2023-05-21 23:38:51 +0300

    dploy

--- Frontend/src/services/host.tsx
+++ Frontend/src/services/host.tsx
@@ -1,6 +1,1 @@
-const status = 'DEV';
-
-export const host =
-  status == 'DEV'
-    ? 'http://localhost:4000'
-    : 'https://school-management-system-9bno.onrender.com';
+export const host = 'https://school-management-system-9bno.onrender.com';
